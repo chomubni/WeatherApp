d27f6f9a3aa11e135bdbe10ec8e89965a8a1810f
ivan kyselov <kyselovivan@gmail.com>
2017-09-26 12:27:30 +0300

    implementing OpenWeatherHelper

--- app/src/main/java/com/example/ivan/weatherapp/database/OpenWeatherHelper.java
+++ app/src/main/java/com/example/ivan/weatherapp/database/OpenWeatherHelper.java
@@ -1,8 +1,25 @@
 package com.example.ivan.weatherapp.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by ivan on 26.09.17.
  */
 
-public class OpenWeatherHelper {
+public class OpenWeatherHelper extends SQLiteOpenHelper {
+    public OpenWeatherHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
